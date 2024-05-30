1db3bfed96fcc25d7c8249dd0e7fb649755da227
Lucas Freire <lucasfreiredev1@gmail.com>
2024-05-30 16:25:42 -0300

    Create Categoria model

--- product/models.py
+++ product/models.py
@@ -1,24 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Categoria(models.TextChoices):
-    CATEGORIAS = {
-    "Celulares": {
-        "smartphone": "Smartphones",
-        "tablet": "Tablets",
-        "acessorio": "Acessorios"
-    },
-    "Computadores": {
-        "notebook": "VHS Tape",
-        "dvd": "DVD",
-    },
-    "unknown": "Unknown",
-}
+    ELETRONICOS = "Eletronicos"
+    COMIDA = "Comidas"
+    CASA = "CASA"
+    COZINHA = "COZINHA"
+    ROUPAS = "ROUPAS"
+    ESPORTE = "ESPORTE"
+    BRINQUEDOS = "BRINQUEDOS"
+    LIVROS = "LIVROS"
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255, default='', blank=False)
     descricao = models.TextField(max_length=1000, default='', blank=False)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     marca = models.CharField(max_length=255, default='', blank=False)
-
+    categoria = models.CharField(max_length=30, choices=Categoria.choices)
+    avaliacoes = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    estoque = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    criadoEm = models.DateTimeField(auto_now_add=True)
